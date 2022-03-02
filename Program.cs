2704aac15d9ad36a1b79ea0942630b5206414d42
wagnerdsodre <wagnerdsodre@gmail.com>
2022-03-02 16:29:42 -0300

    Calcule 3 algarismos multiplicados por si mesmo em uma quantidade N de linhas

--- Program.cs
+++ Program.cs
@@ -1,15 +1,12 @@
 ﻿using System;
+using static System.Console;
 
 namespace desafio1
 {
     class Program
     {
-       
-
-        static void Main(string[] args)
-        {
-
-
+ static void desafio1(){
+     
        /*
     Imprima o por extenso os meses do ano de acordo com seu respectivo
     mês.     
@@ -61,6 +58,58 @@ namespace desafio1
                 Console.WriteLine("Digite um número válido de 1 a 12");
                 break;
         }
+ }
+
+       
+
+        static void Main(string[] args)
+        {
+    
+    /*
+    Desafio
+Você terá o desafio de escrever um programa que leia um valor inteiro N (1 < N < 1000). Este N é a quantidade de linhas de saída que serão apresentadas na execução do programa.
+
+Entrada
+O arquivo de entrada contém um número inteiro positivo N.
+
+Saída
+Imprima a saída conforme o exemplo fornecido.
+
+ 
+Exemplo de Entrada	Exemplo de Saída
+5
+
+1 1 1
+2 4 8
+3 9 27
+4 16 64
+5 25 125
+    
+    
+    
+    */
+
+         WriteLine("Enter the number ?");
+        int n = Int32.Parse(Console.ReadLine());
+
+            int inicio = 1;
+
+            for (int i = 1; i <= n; i++)
+            {
+                
+           WriteLine($@"
+             { i  }  {i * i} { i*i*i  }
+            ");
+                
+            }
+
+
+
+
+
+
+
+
      
         }
     }
