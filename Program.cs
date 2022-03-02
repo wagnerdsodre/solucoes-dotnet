cf3032fb9043cf6a362ff246d8c23c9c7958b271
wagnerdsodre <wagnerdsodre@gmail.com>
2022-03-02 16:21:14 -0300

    Exibindo nome dp mês do ano com switch

--- Program.cs
+++ Program.cs
@@ -10,6 +10,57 @@ namespace desafio1
         {
 
 
+       /*
+    Imprima o por extenso os meses do ano de acordo com seu respectivo
+    mês.     
+    */
+
+
+           Console.WriteLine("Enter the Month") ;
+        int mes = Int32.Parse(Console.ReadLine());
+
+        switch (mes) {
+            case 1:
+                Console.WriteLine("January");
+                break;
+            case 2:
+                Console.WriteLine("February");
+                break;
+            case 3:
+                Console.WriteLine("March");
+                break;
+           
+            case 4:
+                Console.WriteLine("April");
+                break;
+            case 5:
+                Console.WriteLine("May");
+                break;
+            case 6:
+                Console.WriteLine("June");
+                break;
+             case 7:
+                Console.WriteLine("July");
+                break; 
+              case 8:
+                Console.WriteLine("August");
+                break; 
+              case 9:
+                Console.WriteLine("September");
+                break; 
+              case 10:
+                Console.WriteLine("October");
+                break; 
+                case 11:
+                Console.WriteLine("November");
+                break;
+                case 12:
+                Console.WriteLine("December");
+                break;
+                default:
+                Console.WriteLine("Digite um número válido de 1 a 12");
+                break;
+        }
      
         }
     }
