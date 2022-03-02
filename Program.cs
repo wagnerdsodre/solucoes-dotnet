5be3446689315cd9e1536c4fd24aa06ae5480d69
wagnerdsodre <wagnerdsodre@gmail.com>
2022-03-02 16:37:32 -0300

    Calcule a soma de 5 números pares consecutivos a partir de um número N

--- Program.cs
+++ Program.cs
@@ -60,14 +60,11 @@ namespace desafio1
         }
  }
 
-       
-
-        static void Main(string[] args)
-        {
-    
+static void desafio2(){
     /*
     Desafio
-Você terá o desafio de escrever um programa que leia um valor inteiro N (1 < N < 1000). Este N é a quantidade de linhas de saída que serão apresentadas na execução do programa.
+Você terá o desafio de escrever um programa que leia um valor inteiro N (1 < N < 1000). 
+Este N é a quantidade de linhas de saída que serão apresentadas na execução do programa.
 
 Entrada
 O arquivo de entrada contém um número inteiro positivo N.
@@ -84,15 +81,14 @@ Exemplo de Entrada	Exemplo de Saída
 3 9 27
 4 16 64
 5 25 125
-    
+  
     
     
     */
 
          WriteLine("Enter the number ?");
         int n = Int32.Parse(Console.ReadLine());
-
-            int inicio = 1;
+        int inicio = 1;
 
             for (int i = 1; i <= n; i++)
             {
@@ -102,6 +98,90 @@ Exemplo de Entrada	Exemplo de Saída
             ");
                 
             }
+}
+       
+
+        static void Main(string[] args)
+        {
+
+
+/*
+
+O programa deve ler um valor inteiro D indefinidas vezes, 
+ele só irá parar quando o valor de D for igual a 0. 
+Para cada D lido, imprima a soma dos 5 pares consecutivos 
+a partir de D, inclusive ele mesmo , se for par. Se o valor
+ de entrada for 4, por exemplo, a saída deve ser 40, que é 
+ o resultado da operação: 4+6+8+10+12, enquanto que se o valor
+  de entrada for 11, por exempo, a saída deve ser 80, que é a 
+  soma de 12+14+16+18+20.
+
+Entrada
+O arquivo de entrada contém muitos valores inteiros. O último valor do arquivo é zero.
+
+Saída
+Imprima a saida conforme a explicação acima e o exemplo abaixo.
+
+ 
+Exemplo de Entrada	Exemplo de Saída
+4
+11
+0
+
+40
+80
+
+
+
+
+*/
+
+
+
+    
+     int x1 = 4;
+       if(!(x1%2==0)) {
+           x1 = x1+1;
+       }
+      
+       int c1 = x1;
+       int a1 = 0;
+       int qtd1 = 0;
+      while(qtd1 < 5 ){
+         
+       for (int i1 = c1; i1 < x1+10;  i1++ ){
+         
+         if(i1%2 == 0){
+          qtd1++;
+            a1+=i1;
+            
+                
+            } 
+        } 
+    }
+       int x = 11;
+       if(!(x%2==0)) {
+           x = x+1;
+       }
+      
+       int c = x;
+       int a = 0;
+       int qtd = 0;
+      while(qtd < 5 ){
+         
+       for (int i = c; i < x+10;  i++ ){
+         
+         if(i%2 == 0){
+          qtd++;
+            a+=i;
+            
+                
+            } 
+        } 
+    }
+  
+        Console.WriteLine($"{a1}");
+        Console.WriteLine($"{a}");
 
 
 
