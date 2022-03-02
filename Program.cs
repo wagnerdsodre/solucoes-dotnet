ca294ca1928649ac9cd6333b7da3bb842c449a93
wagnerdsodre <wagnerdsodre@gmail.com>
2022-03-02 16:18:23 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace desafio1
+{
+    class Program
+    {
+       
+
+        static void Main(string[] args)
+        {
+
+
+     
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# solucoes-dotnet
